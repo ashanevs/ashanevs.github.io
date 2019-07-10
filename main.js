3e001a51fe7d55433b75012ae1b47e36fe50acb7
Shane Vann-Shirley <ashanev@gmail.com>
2019-07-10 19:38:40 -0400

    improved logic for mobile links, improved link slide animation

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const projects = document.querySelectorAll(".project");
 const mobileLinkBoards = document.querySelectorAll(".mobile-link-board");
 
+mobileNavShow();
+
 function mobileNavShow() {
   if (screen.width < 480) {
     for (let i = 0; i < projects.length; i++) {
       projects[i].addEventListener("click", e => {
         e.preventDefault();
+        visibleCheck();
         mobileLinkBoards[i].classList.toggle("visible");
       });
     }
   }
 }
 
-mobileNavShow();
+function visibleCheck() {
+  for (let i = 0; i < mobileLinkBoards.length; i++) {
+    if (!mobileLinkBoards[i].hasAttribute("visible")) {
+      mobileLinkBoards[i].classList.add("visible");
+    }
+  }
+}
