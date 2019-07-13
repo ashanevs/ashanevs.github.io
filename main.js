d542993a92992562cce979ea640cc3556861c6a5
Shane Vann-Shirley <ashanev@gmail.com>
2019-07-13 19:15:01 -0400

    added email for contact, copy to clipboard function with tooltip

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const projects = document.querySelectorAll(".project");
 const mobileLinkBoards = document.querySelectorAll(".mobile-link-board");
+const emailIcon = document.querySelector(".email-icon");
+const emailLink = document.querySelector(".email-link");
 
 mobileNavShow();
+swapEmail();
+emailClipboard();
 
 function mobileNavShow() {
   if (screen.width < 480) {
@@ -21,4 +25,32 @@ function visibleCheck() {
       mobileLinkBoards[i].classList.add("visible");
     }
   }
+}
+
+function swapEmail() {
+  emailIcon.addEventListener("click", e => {
+    e.preventDefault();
+    emailIcon.style.display = "none";
+    emailLink.style.display = "inline-block";
+    emailLink.style.animation = "fade-in 2s";
+  });
+}
+
+function emailClipboard() {
+  emailLink.addEventListener("click", e => {
+    e.preventDefault();
+    copyStringToClipboard("ashanev@gmail.com");
+    emailLink.setAttribute("data-tooltip", "Copied!");
+  });
+}
+
+function copyStringToClipboard(str) {
+  var el = document.createElement("textarea");
+  el.value = str;
+  el.setAttribute("readonly", "");
+  el.style = { position: "absolute", left: "-9999px" };
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
 }
