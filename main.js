10721fdfb00b818daa3e4c71d38d84f1d60fe253
Shane Vann-Shirley <ashanev@gmail.com>
2019-08-07 09:56:34 -0400

    changed everything

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ const projects = document.querySelectorAll(".project");
 const mobileLinkBoards = document.querySelectorAll(".mobile-link-board");
 const emailIcon = document.querySelector(".email-icon");
 const emailLink = document.querySelector(".email-link");
+const projectTitleContainer = document.querySelectorAll(
+  ".project-title-container"
+);
+const projectTitle = document.querySelector(".project-title");
+const projectContainer = document.querySelector(".project-container");
+const aboutContainer = document.querySelector(".about-container");
+const arrow = document.querySelector(".fa-long-arrow-alt-right");
+const workLink = document.querySelector(".intro-work-link");
+let currentSection = "work";
 
 mobileNavShow();
 swapEmail();
 emailClipboard();
+// addSlide();
+changeSection();
+workLinkFunc();
+// addArrowRotate();
+// removeArrowRotate();
 
 function mobileNavShow() {
   if (screen.width < 480) {
@@ -54,3 +68,66 @@ function copyStringToClipboard(str) {
   document.execCommand("copy");
   document.body.removeChild(el);
 }
+
+// function addSlide() {
+//   projectTitleContainer.addEventListener("click", e => {
+//     e.preventDefault();
+//     // projectContainer.classList.add("slide-far-left");
+//     // projectTitleContainer.classList.toggle("visible");
+//     projectContainer.classList.remove("visible");
+//     arrow.classList.add("rotated");
+//     arrow.classList.remove("unrotated");
+//     removeInfo();
+//   });
+// }
+
+// function removeInfo() {
+//   projectTitleContainer.addEventListener("click", e => {
+//     e.preventDefault();
+//     arrow.classList.remove("rotated");
+//     arrow.classList.add("unrotated");
+//     projectContainer.classList.add("visible");
+//     addSlide();
+//   });
+// }
+
+function changeSection() {
+  for (let i = 0; i < projectTitleContainer.length; i++) {
+    projectTitleContainer[i].addEventListener("click", e => {
+      e.preventDefault();
+      for (let j = 0; j < arrow.length; j++) {
+        if (!arrow[j].hasAttribute("show")) {
+          arrow[j].classList.add("show");
+        }
+      }
+      arrow[i].classList.remove("show");
+      if (projectTitle[i].innerHTML === "My Work") {
+        projectContainer.classList.remove("visible");
+        aboutContainer.classList.add("visible");
+      }
+      if (projectTitle[i].innerHTML === "About") {
+        aboutContainer.classList.remove("visible");
+        projectContainer.classList.add("visible");
+      }
+    });
+  }
+}
+function workLinkFunc() {
+  workLink.addEventListener("click", e => {
+    e.preventDefault();
+    projectTitle.scrollIntoView();
+  });
+}
+
+// function addArrowRotate() {
+//   if (arrow.hasAttribute("rotated")) {
+//   }
+//   workLink.addEventListener("mouseenter", e => {
+//     arrow.classList.add("rotated");
+//   });
+//   workLink.addEventListener("mouseleave", e => {
+//     arrow.classList.add("unrotated");
+//     arrow.classList.remove("rotated");
+//   });
+// }
+// function removeArrowRotate() {}
